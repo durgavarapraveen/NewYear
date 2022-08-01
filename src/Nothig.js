f1b0c530c6a686719914b43a4767a76d364c4662
AteevDuggal <akd3257@gmail.com>
2022-08-01 17:05:13 +0530

    Completed

--- src/Nothig.js
+++ src/Nothig.js
@@ -3,18 +3,6 @@ import Particles from "react-tsparticles";
 import { loadFull } from "tsparticles";
 
 const App = () => {
-  const particlesInit = async (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
-    // starting from v2 you can add only the features you need reducing the bundle size
-    await loadFull(main);
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
   return (
     <div className="App">
       <Particles
@@ -72,6 +60,6 @@ const App = () => {
       />
     </div>
   );
-}
+};
 
 export default App;
